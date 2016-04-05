a21e8d8e8fb0d723d312eaa5a606a4c198e675b2
EDJ12303 <erin.jeffery@comcast.net>
2016-04-05 19:22:54 -0400

    initial commit

--- citibike.py
+++ citibike.py
@@ -5,68 +5,30 @@ Created on Sun Mar 27 20:17:33 2016
 @author: Erin
 """
 import requests
-
-r = requests.get('http://www.citibikenyc.com/stations/json')
-r.json()
-
-key_list = [] #unique list of keys for each station listing
-for station in r.json()['stationBeanList']:
-    for k in station.keys():
-        if k not in key_list:
-            key_list.append(k)
-
-print key_list
-
-# getting the data into a data frame
-
-from pandas.io.json import json_normalize
-
-df = json_normalize(r.json()['stationBeanList'])
-            
-#checking the range of values
 import matplotlib.pyplot as plt
 import pandas as pd
-
-#available bikes distribution
-df['availableBikes'].hist()
-plt.show()
-#total docks distribution
-df['totalDocks'].hist()
-plt.show()
-
-df['totalDocks'].mean()
-print "mean of stations"
-print df['totalDocks'].mean()
-
-#find mean of only the stations that are active
-condition = (df['statusValue'] == 'In Service')
-df[condition]['totalDocks'].mean()
-print "mean of only the stations that are active"
-print df[condition]['totalDocks'].mean()
-
-#find median of the stations 
-
-df['totalDocks'].median()
-print "median of stations"
-print df['totalDocks'].median()
-
-#find median of stations that are active
-df[df['statusValue']== 'In Service']['totalDocks'].median()
-print "median of stations that are active"
-print df[df['statusValue']== 'In Service']['totalDocks'].median()
-
-#storing data in SQLite
-#making a reference table
+from pandas.io.json import json_normalize
 import sqlite3 as lite
+import time
+from dateutil.parser import parse 
+import collections
+
+r = requests.get('http://www.citibikenyc.com/stations/json')
+df = json_normalize(r.json()['stationBeanList'])   
 
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
+# Drop tables if they already exist
+with con:
+    cur.execute("DROP TABLE IF EXISTS citibike_reference")
+    cur.execute("DROP TABLE IF EXISTS available_bikes")
+
 with con:
     cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
     
-#a prepared SQL statement we're going to execute over and over again
-sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+    #a prepared SQL statement we're going to execute over and over again
+    sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
 
 #for loop to populate values in the database
 with con:
@@ -82,38 +44,54 @@ station_ids = ['_' + str(x) + ' INT' for x in station_ids]
 #create the table
 #in this case, we're concatenating the string and joining all the station ids (now with '_' and 'INT' added)
 with con:
-    cur.execute("CREATE TABLE available_bikes ( execution_time INT, " +  ", ".join(station_ids) + ");")
-
-# a package with datetime objects
-import time
-
-# a package for parsing a string into a Python datetime object
-from dateutil.parser import parse 
-
-import collections
+    cur.execute("CREATE TABLE available_bikes (execution_time INT, " +  ", ".join(station_ids) + ");")
 
 #take the string and parse it into a Python datetime object
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
 for i in range(60):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
     exec_time = parse(r.json()['executionTime'])
 
-#create an entry for the execution time by inserting it into the database
-with con:
-    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time,))
     
-#iterate through stations in stationBeanList
-id_bikes = collections.defaultdict(int) 
-#loop through the stations in the station list
-for station in r.json()['stationBeanList']:
-    id_bikes[station['id']] = station['availableBikes']
+    for station in r.json()['stationBeanList']:
+        cur.execute("UPDATE available_bikes SET _%d = %d WHERE execution_time = %s" %(station['id'], station['availableBikes'], exec_time))
+    con.commit()
+    time.sleep(60)
+con.close() #close the database connection when done
+   
+#analyze result
+df = pd.read_sql_query("SELECT * FROM available_bikes ORDER BY execution_time",con,index_col='execution_time')
 
-#iterate through the defaultdict to update the values in the database
-with con:
-    for k, v in id_bikes.iteritems():
-        cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + "WHERE execution_time = " + exec_time.strftime('%s') + ";")
+hour_change = collections.defaultdict(int)
 
-#sleep for one minute 
-time.sleep(60)
-#loop code 60 times
-
+for col in df.columns:
+    station_vals = df[col].tolist()
+    station_id = col[1:] #trim the "_"
+    station_change = 0
+    for k,v in enumerate(station_vals):
+        if k < len(station_vals) - 1:
+            station_change += abs(station_vals[k] - station_vals[k+1])
+    hour_change[int(station_id)] = station_change #convert the station id back to integer
     
-        
+def keywithmaxval(d):
+    """Find the key with the greatest value"""
+    return max(d, key=lambda k: d[k])
+
+# assign the max key to max_station
+max_station = keywithmaxval(hour_change)
+
+#query sqlite for reference information
+cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
+data = cur.fetchone()
+print("The most active station is station id %s at %s latitude: %s longitude: %s " % data)
+print("With %d bicycles coming and going in the hour between %s and %s" % (
+    hour_change[max_station],
+    datetime.datetime.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%dT%H:%M:%S'),
+    datetime.datetime.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%dT%H:%M:%S'),
+))
+
+plt.bar(hour_change.keys(), hour_change.values())
+plt.show()
